efe9d867d352eacb1f226dafe1ff40850752f8b1
jimmy <jimmyguevarag@hotmail.com>
2014-02-10 22:14:24 -0600

    Testiando

---
+++ javascripts/main.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+  // Handler for .ready() called.
+  var wood =$('.wood li');
+  wood.wookmark({
+          
+          autoResize: true, // This will auto-update the layout when the browser window is resized.
+          container: $('.wood-parent'), // Optional, used for some extra CSS styling
+          offset:20 , // Optional, the distance between grid items
+          // outerOffset: 10, // Optional, the distance to the containers border
+          itemWidth: 300 // Optional, the width of a grid item
+      });
+
+
+
+});
+
+
+
+  
+
